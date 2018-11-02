82d3a9b0466ba2e348a378b3e8675462ef559732
Zac Johnson <zejc5f@mail.missouri.edu>
2018-11-02 10:01:17 -0500

    First comment

---
+++ Expenses Core DataTests/Expenses_Core_DataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Expenses_Core_DataTests.swift
+//  Expenses Core DataTests
+//
+//  Created by Zac Johnson on 11/2/18.
+//  Copyright © 2018 ZacJohnson. All rights reserved.
+//
+
+import XCTest
+@testable import Expenses_Core_Data
+
+class Expenses_Core_DataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
